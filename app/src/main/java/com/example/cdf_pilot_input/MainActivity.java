c4debf9f22465e96b4f7e8024d8358365adb54cb
Jennifer Carrera <jennifer.s.carrera@gmail.com>
2021-11-09 14:32:07 -0500

    Use Checkbox Summary

--- app/src/main/java/com/example/cdf_pilot_input/MainActivity.java
+++ app/src/main/java/com/example/cdf_pilot_input/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.cdf_pilot_input;
 
+import android.content.Context;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
- * This app displays an order form to order coffee.
+ * This app collects water testing results and prepares and email for submission
  */
 public class MainActivity extends AppCompatActivity {
-
+/*
     String dateWaterCollected;
     String timeWaterCollected;
     String tabletNumber;
@@ -36,25 +43,89 @@ public class MainActivity extends AppCompatActivity {
     int rottenEggYN;
     int sedimentYN;
     int featheryYN;
-
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitResults(View view) {
-        display(0);
+
+        // Get participant ID number
+        EditText tabletNumberField = (EditText) findViewById(R.id.tablet_number);
+        Editable tabletNumberEditable = tabletNumberField.getText();
+        String tabletNumber = tabletNumberEditable.toString();
+
+        // Get status of water used for drinking checkbox
+        CheckBox drinkingCheckbox = (CheckBox) findViewById(R.id.checkBoxDrinking);
+        boolean usedForDrinking = drinkingCheckbox.isChecked();
+        Log.v("MainActivity", "Used for drinking: " + usedForDrinking);
+
+        // Get status of water used for brushing checkbox
+        CheckBox brushingCheckbox = (CheckBox) findViewById(R.id.checkBoxBrushingTeeth);
+        boolean usedForBrushing = brushingCheckbox.isChecked();
+        Log.v("MainActivity", "Used for brushing: " + usedForBrushing);
+
+        // Get status of water used for handwashing checkbox
+        CheckBox handwashingCheckbox = (CheckBox) findViewById(R.id.checkBoxHandwashing);
+        boolean usedForHandwashing = handwashingCheckbox.isChecked();
+        Log.v("MainActivity", "Used for handwashing: " + usedForHandwashing);
+
+        // Get status of water used for none checkbox
+        CheckBox noneCheckbox = (CheckBox) findViewById(R.id.checkBoxNone);
+        boolean usedForNone = noneCheckbox.isChecked();
+        Log.v("MainActivity", "Used for None: " + usedForNone);
+
+        boolean noneDrinking = usedForDrinking;
+        boolean noneHandwashing = usedForHandwashing;
+        boolean noneBrushing = usedForBrushing;
+
+        if (usedForNone) {
+            noneDrinking = false;
+            noneHandwashing = false;
+            noneBrushing = false;
+        }
+
+        Log.v("MainActivity", "Used for drinking: " + noneDrinking);
+        Log.v("MainActivity", "Used for brushing: " + noneBrushing);
+        Log.v("MainActivity", "Used for handwashing: " + noneHandwashing);
+
+        // Display the results summary
+        String message = createResultsSummary(tabletNumber, noneDrinking, noneBrushing, noneHandwashing, usedForNone);
+
+        // Display results summary to the screen
+        display(message);
     }
 
     /**
-     * This method displays the given quantity value on the screen.
+     * Create summary of the results.
+     *
+     * @param participantID            on the order
+     * @return text summary
      */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.time_running_water);
-        quantityTextView.setText("" + number);
+    private String createResultsSummary(String participantID, Boolean drinking, Boolean brushing, Boolean handwashing, Boolean none) {
+        String summaryMessage = "\n " + participantID;
+        summaryMessage += "\n " + getString(R.string.drinking) + ":" + drinking;
+        summaryMessage += "\n " + getString(R.string.brushing_teeth) + ":" + brushing;
+        summaryMessage += "\n " + getString(R.string.handwashing) + ":" + handwashing;
+        summaryMessage += "\n " + getString(R.string.none) + ":" + none;
+
+        return summaryMessage;
     }
+
+    /**
+     * This method displays the summary to the screen.
+     */
+    private void display(String summary) {
+        TextView summaryTextView = (TextView) findViewById(R.id.summary);
+        summaryTextView.setText("Summary: \n" + summary);
+        summaryTextView.setVisibility(View.VISIBLE);
+
+    }
+
 }
